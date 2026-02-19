73209434ac599447c1f138c224afa22ac27378ea
svjack <ehangzhou@outlook.com>
2026-02-19 20:38:42 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
-from .html_renderer import HTMLFrameRenderer
+from .html_renderer import *
 
 NODE_CLASS_MAPPINGS = {
-    "HTMLFrameRenderer": HTMLFrameRenderer
+    "HTMLFrameRenderer": HTMLFrameRenderer,
+    "HTMLVideoRecorderPlaywright": HTMLVideoRecorderPlaywright
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "HTMLFrameRenderer": "HTML Frame Renderer"
+    "HTMLFrameRenderer": "HTML Frame Renderer",
+    "HTMLVideoRecorderPlaywright": "HTML视频录制器（Playwright版）"
 }
 
 WEB_DIRECTORY = "./js"
